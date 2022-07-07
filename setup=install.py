5396661e852fb47921e898e4e20193d32a16be2f
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2022-07-07 20:12:54 +0530

    Create setup=install.py

---
+++ setup=install.py
@@ -0,0 +1,20 @@
+"aiofiles==0.7.0",  # Async IO for files
+    "blspy==1.0.13",  # Signature library
+    "chiavdf==1.0.6",  # timelord and vdf verification
+    "chiabip158==1.1",  # bip158-style wallet filters
+    "chiapos==1.0.10",  # proof of space
+    "clvm==0.9.7",
+    "clvm_tools==0.4.4",  # Currying, Program.to, other conveniences
+    "chia_rs==0.1.5",
+    "clvm-tools-rs==0.1.9",  # Rust implementation of clvm_tools
+    "aiohttp==3.8.1",  # HTTP server for full node rpc
+    "aiosqlite==0.17.0",  # asyncio wrapper for sqlite, to store blocks
+    "bitstring==3.1.9",  # Binary data management library
+    "colorama==0.4.5",  # Colorizes terminal output
+    "colorlog==6.6.0",  # Adds color to logs
+    "concurrent-log-handler==0.9.19",  # Concurrently log and rotate logs
+    "cryptography==36.0.2",  # Python cryptography library for TLS - keyring conflict
+    "fasteners==0.17.3",  # For interprocess file locking, expected to be replaced by filelock
+    "filelock==3.7.1",  # For reading and writing config multiprocess and multithread safely  (non-reentrant locks)
+    "keyring==23.6.0",  # Store keys in MacOS Keychain, Windows Credential Locker
+    "keyrings.cryptfile==1.3.4",  # Secure storage for keys on Linux (Will be replaced)
